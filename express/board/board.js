3438a80cce606a86596169b7e9843800fd411954
yoonseo232 <ys.bang232@gmail.com>
2024-05-23 14:55:15 +0900

    board

--- express/board/board.js
+++ express/board/board.js
@@ -11,3 +11,8 @@ app.use(cors({
     origin: "http://127.0.0.1:5500",
     credentials:true
 }));
+
+app.set('port', process.env.PORT);
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), "빈 포트에서 서버 실행");
+});
