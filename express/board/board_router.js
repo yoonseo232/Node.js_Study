c1d9c2182e29236a2c80084bbb16d8d39a4be070
yoonseo232 <ys.bang232@gmail.com>
2024-05-30 16:02:10 +0900

    board router

--- express/board/board_router.js
+++ express/board/board_router.js
@@ -1,44 +1,18 @@
+//이거 board 폴더 안에 있어욥~!~~!~!
 //npm install express
 const express = require("express");
-const app = express();
- //npm install dotenv
-const dotenv = require("dotenv").config(); //기본값 .env파일 생성함. 그 안에 설정값 기입
-
- //npm install morgan
-const morgan  = require("morgan");
-app.use(morgan('dev'));
-
- //npm install cors
-const cors    = require("cors");
-app.use(cors({
-    origin: "http://127.0.0.1:5500",
-    credentials:true
-}));
-
-// 포트 설정
-app.set('port', process.env.PORT || 8080 );
-
-app.listen(app.get('port'),()=>{
-    console.log(app.get('port'),"번 포트에서 서버 실행");
-})
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+const router = express.Router();
 
 // 게시글 데이터
 let boardList = [];
 let numOfBoard = 1;
 
 // 게시글 API
-app.get('/',(req,res)=>{
-    res.send("게시글 API 확인");
-});
-
-app.get("/board",(req,res)=>{
+router.get("/",(req,res)=>{
     res.send(boardList);
 });
 
-app.post("/board",(req,res)=>{
+router.post("/",(req,res)=>{
     const board = {
         "id": numOfBoard++,
         "user_id":req.body.user_id,
@@ -50,7 +24,7 @@ app.post("/board",(req,res)=>{
     res.send("글이 등록되었습니다.");
 });
 
-app.delete("/board/:id",(req,res)=>{
+router.delete("/:id",(req,res)=>{
     //req.params.id 값 리스트 삭제
     //찾는 요소 findItem에 넣기
     const findItem = boardList.find((item)=>{
@@ -61,7 +35,7 @@ app.delete("/board/:id",(req,res)=>{
 
     res.send("글이 삭제되었습니다.");
 });
-app.put("/board/:id",(req,res)=>{
+router.put("/:id",(req,res)=>{
     const findItem = boardList.find((item)=>{
         return item.id == req.params.id
     });
@@ -78,5 +52,7 @@ app.put("/board/:id",(req,res)=>{
     boardList.push(board);
     res.send("글이 수정되었습니다.");
 });
+
+module.exports = router;
 
 
---
+++ express/board/board_routerEx.js
@@ -0,0 +1,31 @@
+//이거 board 폴더 안에 있어욥~!~~!~!
+//npm install express
+const express = require("express");
+const app = express();
+ //npm install dotenv
+const dotenv = require("dotenv").config(); //기본값 .env파일 생성함. 그 안에 설정값 기입
+
+ //npm install morgan
+const morgan  = require("morgan");
+app.use(morgan('dev'));
+
+ //npm install cors
+const cors    = require("cors");
+app.use(cors({
+    origin: "http://127.0.0.1:5500",
+    credentials:true
+}));
+
+// 포트 설정
+app.set('port', process.env.PORT || 8080 );
+
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),"번 포트에서 서버 실행");
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const boardRouter = require('./board_router');
+app.use('/board',boardRouter);
+
