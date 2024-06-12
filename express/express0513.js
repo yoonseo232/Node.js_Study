c98d44b18fe713cb23330bd1dcb8f4154e3e461e
yoonseo232 <ys.bang232@gmail.com>
2024-06-12 22:09:48 +0900

    메세지 수정

--- express/express0513.js
+++ express/express0513.js
@@ -8,7 +8,7 @@ app.get("/", (req,res) => {
     if(req.cookies.user){
         res.send("쿠키 데이터가 있습니다. /getUser를 이용해 쿠키를 확인하세요");
     }else{
-        res.send("쿠키 데이터가 있습니다. /setUser를 이용해 쿠키를 확인하세요");
+        res.send("쿠키가 없습니다. /setUser를 사용하여 쿠키를 설정하세요.");
 
     }
 });
